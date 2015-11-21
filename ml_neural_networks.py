c7c6b49c8e4fcec3fd1d6affdbbbea6cd31c0111
wu.fei@outlook.com <ferris@feideMacBook-Pro.local>
2015-11-21 20:06:02 +0800

    remove incomplete file

--- ml_neural_networks.py
+++
@@ -1,6 +0,0 @@
-# how to define network object?
-# consisted of layers
-# each layer should contain: nodes, theta matrix to next layer, delta theta matrix to next layer, z vector from previous layer, a vector from previous layer and intercept 
-# how to initialized a network?
-# how to do fp?
-# how to do bp?
