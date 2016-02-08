e7245cd9d213991bb9594e18c081504e500df30f
wu.fei@outlook.com <wu.fei@outlook.com>
2016-02-08 20:40:57 +0800

    rename

--- ml_NN.py
+++ ml_NN.py
@@ -1,38 +1,38 @@
-# how to define network object?
-# consisted of layers
-# each layer should contain: nodes, theta matrix to next layer, delta theta matrix to next layer,
-# z vector from previous layer, a vector from previous layer and intercept
-# how to initialized a network?
-# how to do fp?
-# how to do bp?
-
 # -*- coding: utf-8 -*-
 """Module docstring.
 
 author: ferris
 update: 2015-11-27
-function: Gaussian Mixtures Model solved with Expectation-Maximization algorithm
 
-Initializing: 每个参数初始化为一个很小的, 接近零的随机值. 例如采用均值为0, 总体方差项为0.01的高斯分布生成
+Initializing: 每个参数初始化为一个很小的, 接近零的随机值. 这里采用高斯分布
 Gradient: 成本函数中的正则化项不包含每一层的截距, 因此截距项与非截距项的梯度表达式不同
 Stopping Criteria: 成本函数减小步长
 
 ref:
 1. http://deeplearning.stanford.edu/wiki/index.php/反向传导算法
+2. http://neuralnetworksanddeeplearning.com/chap2.html
 
 """
 
 import numpy as np
 
 
-class ANN:
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def sigmoid_prime(x):
+    return sigmoid(x) * (1 - sigmoid(x))
+
+
+class NN:
 
     def __init__(self, x, y, target="classification", hl=[4, 5]):
         self.x = x
         self.y = y
         # convert multi-response label to vector
-        if len(np.unique(self.y)) > 2:
-            self.uni = np.unique(self.y)
+        self.uni = np.unique(self.y)
+        if len(self.uni) > 2:
             self.y = np.array([np.where(self.uni == i, 1, 0) for i in self.y])
 
         # shape and problem
@@ -40,15 +40,12 @@ class ANN:
         self.target = target
 
         # number of activation units for each layer (excl bias)
-        # self.layers = hl
-        # self.layers.insert(0, self.n)
-        # ol = 1 if len(np.unique(self.y)) <= 2 else int(len(np.unique(self.y)))
-        # self.layers.append(ol)
         layers = hl.copy()
         layers.insert(0, self.n)
-        ol = 1 if len(np.unique(self.y)) <= 2 else int(len(np.unique(self.y)))
+        ol = 1 if len(self.uni) <= 2 else int(len(self.uni))
         layers.append(ol)
         self.layers = layers
+        self.L = len(self.layers)
 
         # record the fp result
         self.current_y = self.y * 0.0
@@ -56,19 +53,21 @@ class ANN:
         # initialize transforming matrices and biases
         mu = 0.0
         sigma = 0.01
-        self.W = [np.random.normal(mu, sigma, (self.layers[i + 1], self.layers[i]))
-                  for i in range(len(self.layers) - 1)]
-        self.b = [np.random.normal(mu, sigma, (self.layers[i + 1])) for i in range(len(self.layers) - 1)]
+        self.W = [np.random.randn(i, j) for i, j in zip(self.layers[1:], self.layers[:-1])]
+        self.b = [np.random.randn(i, 1) for i in self.layers[1:]]
 
         # initialize gradients
-        self.delta_W = [np.zeros((self.layers[i + 1], self.layers[i])) for i in range(len(self.layers) - 1)]
-        self.delta_b = [np.zeros((self.layers[i + 1])) for i in range(len(self.layers) - 1)]
+        self.delta_W = [np.zeros((i, j)) for i, j in zip(self.layers[1:], self.layers[:-1])]
+        self.delta_b = [np.zeros((i, 1)) for i in self.layers[1:]]
 
-        # initialize values of activation units
-        self.a = [np.zeros(self.layers[i]) for i in range(len(self.layers))]
+        # initialize values of activations
+        self.a = [np.zeros((i, 1)) for i in self.layers]
+        
+        # initialize values of z scores
+        self.z = [np.zeros((i, 1)) for i in self.layers[1:]]
 
         # initialize values of deltas
-        self.delta = [np.zeros(self.layers[i]) for i in range(len(self.layers))]
+        self.delta = [np.zeros((i, 1)) for i in self.layers[1:]]
 
     def fp(self, xj):
         """
@@ -76,14 +75,14 @@ class ANN:
         :param xj: single sample
         :return: update self.a
         """
-        # fp to update the activation values for each layer
-        self.a[0] = xj
+        # convert input layer(1st activation to column array)
+        self.a[0] = xj[np.newaxis].T
 
-        for i in range(1, len(self.a)):
-            z = self.W[i-1].dot(self.a[i-1]) + self.b[i-1]
-            self.a[i] = 1 / (1 + np.exp(-z))
-            # print(self.a[i])
-        # self.current_y[j] = self.a[-1]
+        # update z & activations of each layer
+        for i in range(self.L - 1):
+            z = np.dot(self.W[i], self.a[i]) + self.b[i]
+            self.z[i] = z
+            self.a[i + 1] = sigmoid(z)
 
     def bp(self, yj):
         """
@@ -91,22 +90,22 @@ class ANN:
         :param yj: single response
         :return: update the deltas for each layer, and accumulate gradients for W & b
         """
-        # output layer difference
-        self.delta[-1] = np.multiply(self.a[-1] - yj,
-                                     np.multiply(self.a[-1], 1 - self.a[-1]))
+        # gradients of output layer
+        self.delta[-1] = (self.a[-1] - yj) * sigmoid_prime(self.z[-1])
+        self.delta_b[-1] += self.delta[-1]
+        self.delta_W[-1] += np.dot(self.delta[-1], self.a[-2].T)
 
-        # update deltas
-        for i in list(np.arange(1, len(self.a) - 1)[::-1]):
-            self.delta[i] = np.multiply(np.transpose(self.W[i]).dot(self.delta[i + 1]),
-                                        np.multiply(self.a[i], (1 - self.a[i])))
+        # gradients of previous layers
+        for l in range(2, self.L):
+            z = self.z[-l]
+            sp = sigmoid_prime(z)
+            self.delta[-l] = np.dot(self.W[-l + 1].T, self.delta[-l + 1]) * sp
+            self.delta_b[-l] += self.delta[-l]
+            self.delta_W[-l] += np.dot(self.delta[-l], self.a[-l - 1].T)
 
-        # accumulate gradients. gradients needs to be reset for each descent
-        for i in range(len(self.a) - 1):
-            self.delta_W[i] += np.array([self.delta[i + 1]]).T.dot(np.array([self.a[i]]))
-            self.delta_b[i] += self.delta[i + 1]
-
-    def get_cost(self, lamb=0.01):
+    def get_cost(self, lamb=0.0001):
         """
+        attention: keep lamb small otherwise the training will never converge
         :param lamb: regularization
         :return: value of cost function under current W & b
         """
@@ -114,7 +113,7 @@ class ANN:
             0.5 * lamb * sum([np.sum(i ** 2) for i in self.W])
         return jwb
 
-    def train(self, lamb=0.01, alpha=0.05, maxiter=100):
+    def train(self, lamb=0.0001, alpha=0.05, maxiter=100):
         """
         what's the appropriate stopping criteria???
         :param lamb: regularization
@@ -136,10 +135,10 @@ class ANN:
                 self.W[k] -= alpha * (self.delta_W[k] / self.m + lamb * self.W[k])
                 self.b[k] -= alpha * (self.delta_b[k] / self.m)
             # reset gradients
-            self.delta_W = [np.zeros((self.layers[i + 1], self.layers[i])) for i in range(len(self.layers) - 1)]
-            self.delta_b = [np.zeros((self.layers[i + 1])) for i in range(len(self.layers) - 1)]
+            self.delta_W = [np.zeros((i, j)) for i, j in zip(self.layers[1:], self.layers[:-1])]
+            self.delta_b = [np.zeros((i, 1)) for i in self.layers[1:]]
             # shrink step
-            alpha *= 0.95
+            # alpha *= 0.99
 
         print("training complete")
 
@@ -175,17 +174,14 @@ class ANN:
         return err_rate
 
 # test procedure
-x1 = np.random.multivariate_normal([0, 0, 0], [[2, 0, 1], [0, 3, 0], [1, 0, 2]], 50)
-x2 = np.random.multivariate_normal([1, 0.5, 2], [[1, 0.5, 0.5], [0.5, 2, 1], [0.5, 1, 1]], 50)
-y1 = np.repeat(1, 50)
-y2 = np.repeat(0, 50)
-train_x = np.row_stack((x1, x2))
-train_y = np.concatenate((y1, y2))
-
-N = ANN(train_x, train_y)
-N.train(alpha=1, maxiter=300, lamb=0.01)
-N.predict(train_x)
-N.test(train_x, train_y)
-
-N.fp(train_x[0])
-N.bp(train_y[0])
+# x1 = np.random.multivariate_normal([0, 0, 0], [[2, 0, 1], [0, 3, 0], [1, 0, 2]], 50)
+# x2 = np.random.multivariate_normal([1, 0.5, 2], [[1, 0.5, 0.5], [0.5, 2, 1], [0.5, 1, 1]], 50)
+# y1 = np.repeat(1, 50)
+# y2 = np.repeat(0, 50)
+# train_x = np.row_stack((x1, x2))
+# train_y = np.concatenate((y1, y2))
+#
+# N = NN(train_x, train_y)
+# N.train(alpha=10, maxiter=1500, lamb=0.00001)
+# N.predict(train_x)
+# N.test(train_x, train_y)
